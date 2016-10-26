9b72875cab5d7c58f2b18872380e5af0c8220692
Alena Karpova <ol-olena@mail.ru>
2016-10-26 20:36:36 +0600

    Create new project

---
+++ Basics/Basics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Basics
+//
+//  Created by Alena on 10/26/16.
+//  Copyright (c) 2016 Alena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
